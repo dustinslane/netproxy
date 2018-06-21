918f044683d6dd0cf86eec48283b22cd5a87638c
Dustin Slane <info@dustinslane.nl>
2018-06-21 16:59:36 +0200

    Remove protocol as we are doing both tcp and udp

--- Program.cs
+++ Program.cs
@@ -45,7 +45,6 @@ namespace NetProxy
 
     public class ProxyConfig
     {
-        public string protocol { get; set; }
         public ushort localPort { get; set; }
         public string localIp { get; set; }
         public string forwardIp { get; set; }
